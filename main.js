e2b6bf010aa53dd563f13c15b1753d8f5c9329e9
Jorge Paredes <jopar219@gmail.com>
2015-11-14 13:46:24 -0600

    Remove canvas from main.js

--- main.js
+++ main.js
@@ -9,11 +9,6 @@ window.onload = function(){
 	var player = require('player');
 	var main = new Composition('terrain', terrain, 'player', player);
 
-	var canvas = document.getElementById("mainCanvas");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	var context = canvas.getContext("2d");
-
 	//set to global so it can be accesed by compositions
 	engine = new Engine(context, new Composition('main', main));
 
