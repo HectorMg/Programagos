e34dc50e14be20d8235a5e7ec6e658dc6687a6b5
Jorge Paredes <jopar219@gmail.com>
2015-11-13 21:14:02 -0600

    Fix: Compositions where being loaded before window load

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 //require('main.js');
-var Engine = require('Engine');
-var Composition = require('Composition');
-var terrain = require('terrain');
-var player = require('player');
-var main = new Composition('terrain', terrain, 'player', player);
 
 window.onload = function(){
+	global.document = document;
+	var Engine = require('Engine');
+	var Composition = require('Composition');
+	var terrain = require('terrain');
+	var player = require('player');
+	var main = new Composition('terrain', terrain, 'player', player);
+	//Set window and document for later accesing in node modules.
+	//To access window: require('nw-context').window
+	//To access document: require('nw-context').document
+	//An example is shown in ImageRenderer.js
+	// console.log("asdasds");
+	// var nwContext = require('nw-context');
+	// nwContext.set({
+	//     window: window,
+	//     document: document
+	// });
+
 	var canvas = document.getElementById("mainCanvas");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
