e8687c46677ddf75bc85059410a07f451b7b521d
Jorge Paredes <jopar219@gmail.com>
2015-11-15 12:28:57 -0600

    Add documentation to main.js

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-//require('main.js');
+//main.js
+//Here's the starting point of everything.
 
+//When the window loads, start everything.
 window.onload = function(){
+
+	//Set document to global so it can ba accesed by nodejs. (Anything inside node_modules)
 	global.document = document;
+	//Set window to global so it can be accessed by nodejs.
 	global.window = window;
+
+	//Import Engine.js
 	var Engine = require('Engine');
+
+	//Import Composition.js
 	var Composition = require('Composition');
+
+	//Create start level
 	var terrain = require('terrain');
 	var player = require('player');
-	var main = new Composition('terrain', terrain, 'player', player);
+	var startLevel = new Composition('terrain', terrain, 'player', player);
 
-	//set to global so it can be accesed by compositions
-	global.engine = new Engine(new Composition('main', main));
+	//Create a new engine with the level startLevel and set to global so it can be accesed by nodejs
+	global.engine = new Engine(startLevel);
 
 }
